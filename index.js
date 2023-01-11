6acbbb4752a708c59c13912dc30e544bbb54abdb
Vann Calhoune <vanncalhoune@Vanns-Air.lan>
2023-01-11 07:54:16 -0600

    working on server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || PORT;
+const port = process.env.PORT || 3001;
 const mysql = require('mysql2');
 const cors = require('cors');
 
